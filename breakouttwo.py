31e6a6053e9ba9af0d7e7009349e6684b71f8668
Crystal Shrestha <crystalstha91@gmail.com>
2021-09-03 13:07:00 +0545

    Take Two

--- breakouttwo.py
+++ breakouttwo.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 pygame.init()
 pygame.mixer.init()
 
@@ -32,11 +31,7 @@ paddle_height = 20
 
 all_sprites_list = pygame.sprite.Group()
 
-brick_sound = pygame.mixer.Sound('D:/College/Atari Breakout/sounds_brick.wav')
-paddle_sound = pygame.mixer.Sound('D:/College/Atari Breakout/sounds_paddle.wav')
-wall_sound = pygame.mixer.Sound('D:/College/Atari Breakout/sounds_wall.wav')
 class Brick(pygame.sprite.Sprite):
-
     def __init__(self, color, width, height):
         super().__init__()
         self.image = pygame.Surface([width, height])
@@ -44,7 +39,6 @@ class Brick(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
 class Paddle(pygame.sprite.Sprite):
-
     def __init__(self, color, width, height):
         super().__init__()
         self.image = pygame.Surface([width, height])
@@ -62,7 +56,6 @@ class Paddle(pygame.sprite.Sprite):
             self.rect.x = wall_width
 
 class Ball(pygame.sprite.Sprite):
-
     def __init__(self, color, width, height):
         super().__init__()
         self.image = pygame.Surface([width, height])
@@ -78,13 +71,16 @@ class Ball(pygame.sprite.Sprite):
         self.velocity[0] = self.velocity[0]
         self.velocity[1] = -self.velocity[1]
 
+ball = Ball(white, 10, 10)
+ball.rect.x = WIDTH // 2 - 5
+ball.rect.y = HEIGHT // 2 - 5
+
+
 paddle = Paddle(blue, paddle_width, paddle_height)
 paddle.rect.x = WIDTH // 2 - paddle_width // 2
 paddle.rect.y = HEIGHT - 65
 
-ball = Ball(white, 10, 10)
-ball.rect.x = WIDTH // 2 - 5
-ball.rect.y = HEIGHT // 2 - 5
+
 
 all_bricks = pygame.sprite.Group()
 
@@ -95,7 +91,7 @@ y_gap = 5
 wall_width = 16
 
 def bricks():
-    for j in range(8):
+    for j in range(9):
         for i in range(14):
             if j < 2:
                 if i == 0:
@@ -150,19 +146,17 @@ def bricks():
                     all_sprites_list.add(brick)
                     all_bricks.add(brick)
 
-
 brick_wall = bricks()
 
 all_sprites_list.add(paddle)
 all_sprites_list.add(ball)
 
+def main():
 
-def main(score,balls):
-
-    step = 0
 
     run = True
     while run:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -177,67 +171,21 @@ def main(score,balls):
 
         if ball.rect.y < 40:
             ball.velocity[1] = -ball.velocity[1]
-            wall_sound.play()
 
         if ball.rect.x >= WIDTH - wall_width - 10:
             ball.velocity[0] = -ball.velocity[0]
-            wall_sound.play()
 
         if ball.rect.x <= wall_width:
             ball.velocity[0] = -ball.velocity[0]
-            wall_sound.play()
 
         if ball.rect.y > HEIGHT:
             ball.rect.x = WIDTH // 2 - 5
-            ball.rect.y = HEIGHT // 2 - 5
+            ball.rect.y = HEIGHT // 2- 5
             ball.velocity[1] = ball.velocity[1]
-            balls += 1
-            if balls == 4:
-                font = pygame.font.Font('D:/College/Atari Breakout/DSEG14Classic-Bold.ttf', 70)
-                text = font.render("GAME OVER", 1, white)
-                text_rect = text.get_rect(center=(WIDTH / 2, HEIGHT / 2))
-                screen.blit(text, text_rect)
-                pygame.display.update()
-                pygame.time.wait(2000)
-                run = False
 
-        if pygame.sprite.collide_mask(ball, paddle):
-            ball.rect.x += ball.velocity[0]
-            ball.rect.y -= ball.velocity[1]
-            ball.bounce()
-            paddle_sound.play()
 
-        brick_collision_list = pygame.sprite.spritecollide(ball, all_bricks, False)
-        for brick in brick_collision_list:
-            ball.bounce()
-            brick_sound.play()
-            if len(brick_collision_list) > 0:
-                step += 1
-                for i in range(0, 448, 28):
-                    if step == i:
-                        ball.velocity[0] += 1
-                        ball.velocity[1] += 1
-            if 380.5 > brick.rect.y > 338.5:
-                score += 1
-                brick.kill()
-            elif 338.5 > brick.rect.y > 294:
-                score += 3
-                brick.kill()
-            elif 294 > brick.rect.y > 254.5:
-                score += 5
-                brick.kill()
-            else:
-                score += 7
-                brick.kill()
-            if len(all_bricks) == 0:
-                font = pygame.font.Font('D:/College/Atari Breakout/DSEG14Classic-Bold.ttf', 70)
-                text = font.render("SCREEN CLEARED", 1, white)
-                text_rect = text.get_rect(center=(WIDTH / 2, HEIGHT / 2))
-                all_sprites_list.add(ball)
-                screen.blit(text, text_rect)
-                pygame.display.update()
-                pygame.time.wait(2000)
-                run = False
+
+
 
         screen.fill(black)
 
@@ -271,13 +219,13 @@ def main(score,balls):
         pygame.draw.line(screen, yellow, [(WIDTH - wall_width / 2) - 1, 212.5 + 6 * brick_height + 6 * y_gap],
                          [(WIDTH - wall_width / 2) - 1, 212.5 + 8 * brick_height + 8 * y_gap], wall_width)
 
-        all_sprites_list.update()
+        all_sprites_list.draw(screen)
 
         pygame.display.update()
 
         clock.tick(FPS)
 
-
     pygame.quit()
 
-main(score,balls)
+
+main()
